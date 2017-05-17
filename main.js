75da5145c6e4022fe38b4731a3ba2890fc76e8b1
C0D1NGD0J0 <web-dev@United4Life.local>
2017-05-17 10:40:02 +0100

    toggleAll method completed

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 'use strict';
-/* Version Five
+/* Version Six
 	 Requirements:
-		=> displayTodos should show todoText
-		=> displayTodos should tell you if todos is empty
-		=> displayTodos should show completed
+		=> toggleAll: If everything's true, make everything false
+		=> toggleAll: Otherwise, make everything true.
 */
 
 // SOLUTION
 let todoList = {
-	todos: [{todoText: 'item1', completed: false}, {todoText: 'item2', completed: true}, {todoText: 'item3', completed: false}],
+	todos: [{todoText: 'item1', completed: true}, {todoText: 'item2', completed: true}, {todoText: 'item3', completed: true}],
 	
 	displayTodos: function() {
 		if(this.todos.length > 0){
@@ -41,8 +40,33 @@ let todoList = {
 		let todo = this.todos[position];
 		todo.completed = !todo.completed;
 		this.displayTodos();
+	},
+
+	toggleAll: function(){
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+
+		// Get number of completed todos
+		for(let i = 0; i < totalTodos; i++){
+			if(this.todos[i].completed === true){
+				completedTodos++;
+			}
+		}		
+		//If everything is true, make everything false
+		if(completedTodos === totalTodos){
+			for(let i = 0; i < totalTodos; i++){
+				this.todos[i].completed = false;
+			}
+		} else {
+			//If everything is false, make everything true
+			for(let i = 0; i < totalTodos; i++){
+				this.todos[i].completed = true;
+			}
+		}
+			
+		this.displayTodos();
 	}
-}
+};
 
 console.log("================Display Todos Object================");
 todoList.displayTodos();
